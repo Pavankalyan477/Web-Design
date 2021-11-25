324f382318b14ebcdc9634f81b8692a1db0d2eff
Pavankalyan477 <sarakantipavankalyan070@gmail.com>
2021-11-25 23:13:10 +0530

    ASSIGNMENTS

--- PORTFOLIO/my_portfolio/src/components/All/About.jsx
+++ PORTFOLIO/my_portfolio/src/components/All/About.jsx
@@ -14,8 +14,7 @@ const About = () => {
           <h1 className="section-title">About <span>me</span></h1>
           <h2 className="role">Full Stack Web Developer</h2>
           <p className="myself">Dedicated and Efficient Full-stack Web Developer with a passion to
-            build and optimize user-focused websites. Self-motivated and task driven professional with a knack to learn new technologies. I believe in the power of programming to transform and improve the lives of people around the world.
-</p>
+            build and optimize user-focused websites. Self-motivated and task driven professional with a knack to learn new technologies. I believe in the power of programming to transform and improve the lives of people around the world.</p>
            
           <a href={resume} className="cta">Download Resume</a>
         </div>
